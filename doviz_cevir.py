84440338576b27c3e404e9a0cbb819a1a5b443b1
Toshiba <ozdemirkubraa11@gmail.com>
2021-02-27 11:43:29 -1200

    efektif dolar satış fıyatını çekebiliyorum.

---
+++ doviz_cevir.py
@@ -0,0 +1,27 @@
+# TCMB sitesinden efektif dolar satış fiyatını çekelim.
+# https://www.turkiye.gov.tr/doviz-kurlari
+
+from bs4 import *
+import requests
+
+sayfa = requests.get("https://www.turkiye.gov.tr/doviz-kurlari")
+
+#print(sayfa.text)
+
+soup = BeautifulSoup(sayfa.text, "html.parser")
+
+efektif_dolar_satis = soup.find('td', text='1 ABD DOLARI')\
+                          .find_next_sibling('td')\
+                          .find_next_sibling('td')\
+                          .find_next_sibling('td')\
+                          .find_next_sibling('td')\
+                          .text
+
+print('efektif_dolar_satis', efektif_dolar_satis)
+
+
+
+
+
+
+
